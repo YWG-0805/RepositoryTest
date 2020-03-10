8e8512b18e28768d3a73d81f15666f08c9e9f8e2
1039343278@qq.com <xiao0819sdo>
2020-03-10 22:25:18 +0800

    提交java工程

---
+++ HelloJava/src/main/java/cn/itheima/hellojava/App.java
@@ -0,0 +1,13 @@
+package cn.itheima.hellojava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
